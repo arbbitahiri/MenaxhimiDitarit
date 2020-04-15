075ae3726edda7b34a3e6c43d82a13c106f09bcd
Arb Tahiri <arb.tahiri@riinvest.net>
2020-04-15 14:24:48 +0200

    Interface dhe Auditi

---
+++ MenaxhimiDitarit.BO/BaseAuditObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenaxhimiDitarit.BO
+{
+    public class BaseAuditObject
+    {
+        public string InsertBy { get; set; }
+        public string InsertDate { get; set; }
+        public string UpdateBy { get; set; }
+        public string UpdateDate { get; set; }
+        public int UpdateNo { get; set; }
+    }
+}
---
+++ MenaxhimiDitarit.BO/Interface/IBaseCRUD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenaxhimiDitarit.BO.Interface
+{
+    public interface IBaseCRUD<T>
+    {
+        bool Add(T model);
+        bool Update(T model);
+        bool Remove(T model);
+        List<T> GetAll();
+    }
+}
